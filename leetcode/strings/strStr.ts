9986c806e72178d8dcfd0412dc9b0bef9cdd7fd1
* Laila Atrmouh <leiluspocus@users.noreply.github.com>
2026-04-02 17:59:17 +0100

    kata - implement strStr (cheated ^^')

---
+++ leetcode/strings/strStr.ts
@@ -0,0 +1,6 @@
+function strStr(haystack: string, needle: string): number { 
+    return haystack.indexOf(needle) 
+};
+
+console.log(strStr("sxxbutsad", "sad"))
+console.log(strStr("leetcode", "leeto"))
