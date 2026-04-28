95f1998755e16d9e84fb7f543d501d9b695edb3d
* Laila Atrmouh <leiluspocus@users.noreply.github.com>
2026-04-28 15:19:28 +0200

    kata: fizzbuzz basic

---
+++ leetcode/maths/fizzbuzz.ts
@@ -0,0 +1,19 @@
+function fizzBuzz(n: number): string[] {
+    const result: string[] = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i%5 === 0) {
+            result.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            result.push("Fizz");
+        } else if (i % 5 === 0) {
+            result.push("Buzz");
+        } else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+};
+
+console.log(fizzBuzz(3)); // ["1","2","Fizz"]
+console.log(fizzBuzz(5)); // ["1","2","Fizz","4","Buzz"]
+console.log(fizzBuzz(15)); // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
